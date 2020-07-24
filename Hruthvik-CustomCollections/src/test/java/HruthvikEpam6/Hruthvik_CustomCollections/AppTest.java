058cc40ea93241c9d532f6ec36679bc4f6edb835
hruthvikpasumarthy <hruthvik.pasumarthy@gmail.com>
2020-07-24 17:09:03 +0530

    adding

---
+++ Hruthvik-CustomCollections/src/test/java/HruthvikEpam6/Hruthvik_CustomCollections/AppTest.java
@@ -0,0 +1,20 @@
+package HruthvikEpam6.Hruthvik_CustomCollections;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
